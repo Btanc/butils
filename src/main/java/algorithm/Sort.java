0ffc54b9b86abc8fe50eb5aff0f19ac525e5a61d
Your Btanc <478807919@qq.com>
2019-09-13 22:14:54 +0800

    init project

---
+++ src/main/java/algorithm/Sort.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.io.Serializable;
+
+/**
+ * This is a class that contains sorting methods
+ *
+ * @author Btanc
+ */
+public class Sort implements Serializable {
+
+    private static final long serialVersionUID = -5980521921395336744L;
+
+    /**
+     *
+     *
+     * @param ${tags}
+     * @author Btanc
+     */
+    public static void sort(int[] arrays) {
+
+    }
+}
