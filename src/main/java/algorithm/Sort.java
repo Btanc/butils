24272a9dca553b751934f358d432d1ea007886bd
Btanc <478807919@qq.com>
2019-09-14 02:19:12 +0800

    chore: design structure

--- src/main/java/algorithm/Sort.java
+++ src/main/java/algorithm/Sort.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * This is a class that contains sorting methods
@@ -14,10 +15,61 @@ public class Sort implements Serializable {
     /**
      *
      *
+     * @param array This is the array of integer to be sorted
+     * @author Btanc
+     */
+    public static void sort(int[] array) {
+
+        // TODO
+    }
+
+    /**
+     *
+     *
+     * @param array This is the array of long integer to be sorted
+     * @author Btanc
+     */
+    public static void sort(long[] array) {
+        // TODO
+    }
+
+    /**
+     *
+     *
+     * @param array This is the array of single-precision floating-point to be sorted
+     * @author Btanc
+     */
+    public static void sort(float[] array) {
+        // TODO
+    }
+
+    /**
+     *
+     *
+     * @param array This is the array of double-precision floating-point to be sorted
+     * @author Btanc
+     */
+    public static void sort(double[] array) {
+        // TODO
+    }
+
+    /**
+     *
+     *
+     * @param array This is the array of character to be sorted
+     * @author Btanc
+     */
+    public static void sort(char[] array) {
+        // TODO
+    }
+
+    /**
+     *
+     *
      * @param ${tags}
      * @author Btanc
      */
-    public static void sort(int[] arrays) {
+    public static void quickSort() {
 
     }
 }
